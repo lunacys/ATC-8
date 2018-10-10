26a522b96aa582ef293e9e5c47d5582b1db07ba1
G.Alfyorov <g.alfyorov@forexdevelopment.com>
2018-10-10 16:38:48 +0500

    Add project files.

---
+++ ATC-8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATC_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
