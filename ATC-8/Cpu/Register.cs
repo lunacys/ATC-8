a76731195ab61d9049bb9f8e9c3cb9302b5e7a01
lunacys <loonacuse@gmail.com>
2018-10-23 12:20:03 +0500

    Make aliases for ValueHigh and ValueLow for more comfortable use

--- ATC-8/Cpu/Register.cs
+++ ATC-8/Cpu/Register.cs
@@ -7,10 +7,27 @@ namespace ATC8.Cpu
         public RegisterName Name { get; }
         public Word Value { get; set; }
 
+        public byte ValueHigh
+        {
+            get => Value.ValueHigh;
+            set => Value = new Word(value, Value.ValueLow);
+        }
+
+        public byte ValueLow
+        {
+            get => Value.ValueLow;
+            set => Value = new Word(Value.ValueHigh, value);
+        }
+
         public Register(RegisterName name, Word value)
         {
             Name = name;
             Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}";
         }
     }
 }
