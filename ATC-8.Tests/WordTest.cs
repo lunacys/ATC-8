1bc971f01ba3ef7d841d5d4d598c7f01246cafd3
lunacys <loonacuse@gmail.com>
2018-10-26 17:53:33 +0500

    Add placeholder for Word tests

---
+++ ATC-8.Tests/WordTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ATC_8.Tests
+{
+    [TestFixture]
+    public class WordTest
+    {
+        [Test]
+        public void WordUsage_Test()
+        {
+
+        }
+    }
+}
