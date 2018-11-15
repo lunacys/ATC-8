db035ccc8e937d5a10bae16267e03eace301b13a
lunacys <loonacuse@gmail.com>
2018-11-15 15:58:59 +0500

    Update Interpreter

--- ATC-8.Interpreter/Program.cs
+++ ATC-8.Interpreter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using ATC8.Cpu;
 using ATC8.VirtualMachine;
 using ATC8.VirtualMachine.Lexer.Tokens;
@@ -9,6 +10,96 @@ namespace ATC8.Interpreter
     {
         static void Main(string[] args)
         {
+            Tests();
+
+            Console.WriteLine("Run your ATC-8 Assembly commands: ");
+
+            try
+            {
+                var parser = new Parser();
+                var vm = new VirtualMachine.VirtualMachine();
+
+                while (true)
+                {
+                    var currLine = Console.ReadLine();
+                    if (string.IsNullOrEmpty(currLine))
+                    {
+                        Console.WriteLine("The line is empty");
+                        continue;
+                    }
+
+                    if (currLine.ToLower() == "help" || currLine.ToLower() == "?")
+                    { }
+                    else if (currLine.ToLower() == "quit" || currLine.ToLower() == "exit" || currLine.ToLower() == "q")
+                        break;
+
+                    bool shouldViewBytecode = false;
+                    if (currLine.EndsWith("|d"))
+                    {
+                        shouldViewBytecode = true;
+                        currLine = currLine.Replace("|d", "");
+                    }
+
+                    Word[] bytecode;
+
+                    try
+                    {
+                        bytecode = parser.ParseString(currLine);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+                    
+                    if (shouldViewBytecode)
+                    {
+
+                        var maxBytesOnLine = 20;
+                        
+                        Console.WriteLine();
+
+                        for (int i = 0; i < bytecode.Length; i++)
+                        {
+                            if (maxBytesOnLine != 0 && i != 0 && i % maxBytesOnLine == 0)
+                                Console.WriteLine();
+                            Console.Write($"{((byte) bytecode[i]).ToHexString().ToUpper()} ");
+                        }
+
+                        Console.WriteLine("\n");
+                    }
+
+                    Console.WriteLine("Interpreting: ");
+
+                    try
+                    {
+                        vm.Interpret(bytecode);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            
+
+            Console.WriteLine("Success! Press any key to continue..");
+            Console.ReadKey();
+        }
+
+        [Conditional("DEBUG")]
+        private static void Tests()
+        {
+            Console.WriteLine(" ===== TESTS ===== \n");
+
             var ax = new Register(RegisterName.Ax, 0xFA); // 0b11111010
             var bx = new Register(RegisterName.Bx, 0x00); // 0b00000000
             var cx = new Register(RegisterName.Cx, 0xFF); // 0b11111111
@@ -33,7 +124,6 @@ namespace ATC8.Interpreter
             }
 
             Console.WriteLine();
-
 
             try
             {
@@ -66,9 +156,7 @@ namespace ATC8.Interpreter
                 Environment.Exit(1);
             }
 
-
-            Console.WriteLine("Success! Press any key to continue..");
-            Console.ReadKey();
+            Console.WriteLine(" ===== END TESTS ===== \n");
         }
     }
 }
